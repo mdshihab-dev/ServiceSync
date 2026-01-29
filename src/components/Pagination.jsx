9e5bb61e3af7d26a5ee5682bf0076020395ae548
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2026-01-29 17:43:54 +0600

    update

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,7 @@
 import Table from './Table'
 const Pagination = () => {
   return (
-    <section className='mt-5 md:mt-8'>
+    <section className='mt-5 w-screen md:w-full md:mt-8'>
         <Table/>
     </section>
   )
